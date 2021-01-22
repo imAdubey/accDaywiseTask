61de6c66c8c0afe4bbcc3a6ef751d5278a843049
Abhishek Dubey <48856050+imAdubey@users.noreply.github.com>
2021-01-22 17:02:20 +0530

    Update users.js

--- node-sequelize/routes/users.js
+++ node-sequelize/routes/users.js
@@ -1,65 +1,83 @@
+/* eslint-disable prefer-destructuring */
 const express = require('express');
+
 const router = express.Router();
 
 const {findEmp, saveEmp, updateEmp, deleteEmp} = require('../service/v1/user/user');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  findEmp().then((data)=>{
-    res.status(200).json({
-      message: "OK",
-      result: data
-    });
+router.get('/', (req, res) => {
+  findEmp().then((data) => {
+    res.render('index', {userData: data});
+    // res.status(200).json({
+    //   message: 'OK',
+    //   result: data,
+    // });
+    // }).catch((err) => {
+    //   res.status(500).json({
+    //     message: err,
+    //   });
+    // });
   }).catch((err)=>{
-    res.status(500).json({
-      message: err
-    });
+    res.send(err);
   });
 });
 
-router.post('/', (req, res)=>{
+router.post('/addUser', (req, res) => {
   const body = req.body;
-  saveEmp(body).then((data)=>{
-    res.status(200).json({
-      message: "Saved!",
-      result: data
-    });
+  saveEmp(body).then((data) => {
+    res.redirect('/users/');
+  //   res.status(200).json({
+  //     message: 'Saved!',
+  //     result: data,
+  //   });
+  // }).catch((err) => {
+  //   // console.error(err);
+  //   res.status(500).json({
+  //     message: err,
+  //   });
+  // });
   }).catch((err)=>{
-    // console.error(err);
-    res.status(500).json({
-      message: err
-    });
+    res.send(err);
   });
 });
 
-router.put('/updateEmployee/:id', (req, res)=>{
-  const id = req.params.id;
+router.post('/updateEmployee', (req, res) => {
+  const id = req.body.id;
   const body = req.body;
-  updateEmp(id, body).then((data)=>{
-    res.status(200).json({
-      message: "Updated!",
-      result: data
-    });
+  updateEmp(id, body).then((data) => {
+    res.redirect('/users/');
+  //   res.status(200).json({
+  //     message: 'Updated!',
+  //     result: data,
+  //   });
+  // }).catch((err) => {
+  //   // console.error(err);
+  //   res.status(500).json({
+  //     message: err,
+  //   });
+  // });
   }).catch((err)=>{
-    // console.error(err);
-    res.status(500).json({
-      message: err
-    });
+    res.send(err);
   });
 });
 
-router.delete('/deleteEmployee/:id', (req, res)=>{
+router.get('/deleteEmployee/:id', (req, res) => {
   const id = req.params.id;
-  deleteEmp(id).then((data)=>{
-    res.status(200).json({
-      message: "Deleted!",
-      result: data
-    });
+  deleteEmp(id).then((data) => {
+    res.redirect('/users/');
+  //   res.status(200).json({
+  //     message: 'Deleted!',
+  //     result: data,
+  //   });
+  // }).catch((err) => {
+  //   // console.error(err);
+  //   res.status(500).json({
+  //     message: err,
+  //   });
+  // });
   }).catch((err)=>{
-    // console.error(err);
-    res.status(500).json({
-      message: err
-    });
+    res.send(err);
   });
 });
 
